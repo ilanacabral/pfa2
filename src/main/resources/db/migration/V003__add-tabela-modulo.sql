928c13ee0aad86693ecf10c73f831c4b780af279
Ilana <iccphbcloud@gmail.com>
2021-05-01 12:46:42 -0300

    Incluindo registros iniciais no banco de dados.

---
+++ src/main/resources/db/migration/V003__add-tabela-modulo.sql
@@ -0,0 +1,4 @@
+INSERT INTO modulo (id, nome) VALUES (1,"Docker");
+INSERT INTO modulo (id, nome) VALUES (2,"Kubenertes");
+INSERT INTO modulo (id, nome) VALUES (3,"DDD");
+INSERT INTO modulo (id, nome) VALUES (4,"Git");
